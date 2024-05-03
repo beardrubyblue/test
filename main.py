633fc1a59c562c061588cab380fba9b0dacf4457
ugodina-elizaveta <ugodina.elizaveta@gmail.com>
2024-05-03 18:57:18 +0300

    test

--- main.py
+++ main.py
@@ -247,7 +247,7 @@ def convert_to_binary_data(filename):
 def supremacy(email, password):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True, args=["--disable-blink-features=AutomationControlled"])
-        context = browser.new_context(ignore_https_errors=True, accept_downloads=True, user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/124.0.0.0')
+        context = browser.new_context(user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36')
         page = context.new_page()
         logging.critical("Browser is open!")
 
@@ -268,13 +268,12 @@ def supremacy(email, password):
         page.click('#passwordNext')
         logging.critical("Authorization completed!")
 
-        page.screenshot(path="screenshot.png", full_page=True)
-        sqlite_insert_blob_query = """INSERT INTO "Elizaveta".screenshot
-                                  (photo) VALUES (?)"""
-        emp_photo = convert_to_binary_data("screenshot.png")
-        data_tuple = (emp_photo,)
-        DBC.execute(sqlite_insert_blob_query, data_tuple)
-        DB.commit()
+        # page.screenshot(path="screenshot.png", full_page=True)
+        # sqlite_insert_blob_query = 'INSERT INTO "Elizaveta".screenshot(photo) VALUES (?)'
+        # emp_photo = convert_to_binary_data("screenshot.png")
+        # data_tuple = (emp_photo,)
+        # DBC.execute(sqlite_insert_blob_query, data_tuple)
+        # DB.commit()
 
         page.wait_for_timeout(2000)
         page.click('#plusButton')
