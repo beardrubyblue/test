0c0b0bcc1d20463d7caf033f242228fcbf9b9b3d
ugodina-elizaveta <ugodina.elizaveta@gmail.com>
2024-05-09 09:33:51 +0300

    test

--- main.py
+++ main.py
@@ -360,6 +360,9 @@ def supremacy():
             if 'Вход в сервис "supremacy.info"' in element.text_content().strip():
                 page.click('button[class="VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-INsAgc VfPpkd-LgbsSe-OWXEXe-dgl2Hf Rj2Mlf OLiIxf PDpWxe P62QJc LQeN7 BqKGqe pIzcPc TrZEUc lw1w4b"]')
                 logging.critical("Next")
+            elif 'wants to access your Google Account' in element.text_content().strip():
+                page.click('#submit_deny_access')
+                logging.critical("Next")
             else:
                 page.click('#idvPreregisteredPhoneNext')
                 logging.critical("Next")
