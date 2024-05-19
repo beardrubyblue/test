5a60d3fc76f7d91002b1d233f062b3b15f0f0955
AlanD <avlasenko@arbat.dev>
2024-05-19 20:33:37 +0300

    gmail reg

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ CC = {
     'recaptchaTimeout': 600,
     'pollingInterval': 10}
 SOLVER = TwoCaptcha(**CC)
+KIND_ID = 3
 HEADERS = {}
 with open('Names.txt', 'r') as F:
     Names = F.readlines()
@@ -434,6 +435,270 @@ def main():
     """Версия проекта."""
     html = 'Проект: VKReger<BR>Версия: 26.04.2024 17:18'
     return HTMLResponse(content=html, status_code=200)
+
+
+@app.get("/reg_acc")
+async def reg_acc():
+    users = await make_request('get', f'https://accman-odata.arbat.dev/get-innocent-humanoid?kind_id={KIND_ID}')
+    users = json.loads(users)
+    proxy_list = await create_proxy_list()
+    if len(proxy_list) == 0:
+        finish('There Are No Proxies Found! Waiting 1000 Seconds Before Exit.')
+    logging.critical(len(proxy_list))
+    pr = proxy_list[1]
+    user_agent = UserAgent()
+    user_agent = user_agent.random
+    proxy = {
+        'server': f'http://{pr["host"]}:{str(pr["port"])}',
+        "username": pr['login'],
+        "password": pr['password']
+    }
+    logging.critical(proxy)
+    async with async_playwright() as playwright:
+        chromium = playwright.firefox
+        browser = await chromium.launch(headless=False)
+        context = await browser.new_context(user_agent=user_agent, proxy=proxy)
+        page = await context.new_page()
+        response = await mine(context, page, users, pr)
+        await browser.close()
+        logging.critical(response)
+    return response
+
+
+async def create_proxy_list(kind: int = 5, ptype: str = 3, country: str = 'RU', max_amount: int = 10000):
+    """Функция создаёт список из URL-строк прокси вида type://login:password@host:port. Прокси для этого берутся с одного из сайтов: [https://free-proxy-list.net или https://www.sslproxies.org] [https://proxy-manager.arbat.dev] [https://proxy6.net]."""
+    proxy_list = []
+    pt = ''
+    if ptype == 1:
+        pt = 'socks4://'
+    if ptype == 2:
+        pt = 'socks5://'
+    if ptype == 3:
+        pt = 'http://'
+    # Получение бесплатных https прокси случайных стран с сайтов [https://free-proxy-list.net или https://www.sslproxies.org].
+    if kind == 1 and ptype == 3:
+        async with aiohttp.ClientSession() as session:
+            response = await session.get('https://free-proxy-list.net')
+            soup = BeautifulSoup(response.content, 'html.parser')
+            for row in soup.find('table', attrs={'class': 'table table-striped table-bordered'}).find_all('tr')[1:]:
+                tds = row.find_all('td')
+                if tds[6].text.strip() == 'yes':
+                    proxy_list.append(f'{pt}{tds[0].text.strip()}:{tds[1].text.strip()}')
+    # Получение платных https или socks5 прокси указанной страны из объединения proxy6_net_pool сайта [https://proxy-manager.arbat.dev].
+    if kind == 2 and ptype in [2, 3]:
+        params = {'pool_id': '9f687b07-b5f5-4227-9d04-4888ac5be496', 'limit': 10000, 'sla': '0.7'}
+        async with aiohttp.ClientSession() as session:
+            response = await session.get('https://proxy-manager.arbat.dev/pools/9f687b07-b5f5-4227-9d04-4888ac5be496/proxies', params=params)
+            jd = json.loads(await response.text(errors='replace'))
+            for proxy in jd:
+                if proxy['proxy']['proxy_type'] == ptype and proxy['proxy']['country_code'] == country:
+                    proxy_list.append(f"{pt}{proxy['proxy']['login']}:{proxy['proxy']['password']}@{proxy['proxy']['host']}:{proxy['proxy']['port']}")
+    # Получение одной статичной эксклюзивной русской https или socks5 прокси с сайта [https://proxy6.net].
+    if kind == 3 and ptype in [2, 3]:
+        proxy_list.append(f'{pt}{configs.ProxyUserOfKind3}@193.187.144.37:8000')
+    # Получение платных https прокси указанной страны из объединения playwright сайта [https://proxy-manager.arbat.dev].
+    if kind == 4 and ptype == 3:
+        params = {'limit': 10000, 'sla': '0.7'}
+        async with aiohttp.ClientSession() as session:
+            response = await session.get('https://proxy-manager.arbat.dev/pools/b4485d4d-d678-4b5f-9ada-b972db01764b/proxies', params=params)
+            jd = json.loads(await response.text(errors='replace'))
+            for proxy in jd:
+                if proxy['proxy']['proxy_type'] == ptype and proxy['proxy']['country_code'] == country:
+                    proxy_list.append(f"{pt}{proxy['proxy']['login']}:{proxy['proxy']['password']}@{proxy['proxy']['host']}:{proxy['proxy']['port']}")
+    # Быстрое получение одной платной https или socks5 прокси указанной страны из объединения proxy6_net_pool сайта [https://proxy-manager.arbat.dev], которая дольше всех не использовалась.
+    if kind == 5 and ptype in [2, 3]:
+        params = {'pool_id': '9f687b07-b5f5-4227-9d04-4888ac5be496', 'sla': '0.7', "country": country}
+        async with aiohttp.ClientSession() as session:
+            response = await session.get('https://proxy-manager.arbat.dev/proxies/use', params=params)
+            jd = json.loads(await response.text(errors='replace'))
+            proxy = jd['proxy']
+            proxy_list.append(f"{pt}{proxy['login']}:{proxy['password']}@{proxy['host']}:{proxy['port']}")
+    random.shuffle(proxy_list)
+    return proxy_list[:max_amount]
+
+
+async def make_request(method: str, url: str, params: Dict = None, headers: Dict = None, cookies: Dict = None,
+                       timeout: int = 60):
+    async with aiohttp.ClientSession() as session:
+        if method == 'get':
+            async with session.get(url, params=params, headers=headers, cookies=cookies, timeout=timeout) as resp:
+                response = await resp.text(errors='replace')
+        if method == 'put':
+            async with session.put(url, params=params, headers=headers, cookies=cookies, timeout=timeout) as resp:
+                response = await resp.text(errors='replace')
+    return response
+
+
+def finish(reason: str, timeout: int = 100000000):
+    logging.critical(reason)
+    logging.critical(
+        'Finished At: ' + str(datetime.datetime.now()) + ' Waiting For: ' + str(timeout) + ' Seconds Before Exit.')
+    time.sleep(timeout)
+    exit(666)
+
+
+async def get_sms(phone_jd):
+    url = 'http://10.9.20.135:3000/phones/messages/' + str(phone_jd['phone']) + '?fromTs=0' + str(
+        phone_jd['listenFromTimestamp'])
+    response = await make_request('get', url)
+    logging.critical(response)
+    return response
+
+
+def generate_gmail(first_name, last_name, year):
+    first_name = translit(first_name, 'ru', reversed=True)
+    last_name = translit(last_name, 'ru', reversed=True)
+    gmail = f'{first_name.lower()}.{last_name.lower()}{year + str(random.randint(1, 999))}'
+    gmail = gmail.replace('`', '')
+    return gmail
+
+
+def generate_pass(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+
+async def send_acc(phone_jd, password, proxy, first_name, last_name, birthday, humanoid_id, last_cookies):
+    proxy = proxy['host'] + ':' + str(proxy['port']) + '|' + proxy['country_code'] + ' ' + proxy['id']
+    data = {
+        'kind_id': KIND_ID,
+        'phone': phone_jd['phone'],
+        'password': password,
+        'info': {
+            'Proxy': proxy,
+            'FieldName': first_name,
+            'Surname': last_name,
+            'Birthday': birthday
+        },
+        "humanoid_id": humanoid_id,
+        "last_cookies": last_cookies
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.post('https://accman-odata.arbat.dev/create', json=data) as response:
+            rr = await response.text()
+            logging.critical(rr)
+            return rr
+
+
+async def mine(context, page, users, proxy):
+    humanoid_id = users['id']
+    first_name = users['first_name']
+
+    last_name = users['last_name']
+
+    phone_jd = await make_request('get', 'http://10.9.20.135:3000/phones/random?service=gmail&bank=virtual')
+    phone_jd = json.loads(phone_jd)
+    phone_string = '+' + phone_jd['phone'][0] + ' ' + phone_jd['phone'][1:4] + ' ' + phone_jd['phone'][4:7] + '-' + \
+                   phone_jd['phone'][7:9] + '-' + phone_jd['phone'][9:11]
+
+    day = str(users['birth_date'].split('-')[2])
+    month = int(users['birth_date'].split('-')[1])
+    year = str(users['birth_date'].split('-')[0])
+
+    if users['sex'] == 'female':
+        sex = 1
+    else:
+        sex = 2
+
+    gmail = generate_gmail(first_name, last_name, year)
+
+    password = generate_pass(random.randint(10, 15))
+
+    await page.goto('https://google.com/')
+    await page.click('.gb_Ca')
+    await page.wait_for_timeout(2000)
+    await page.locator('xpath=//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div[2]/div/div/div[1]/div/button').click()
+    await page.wait_for_timeout(200)
+    await page.locator('xpath=//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div[2]/div/div/div[2]/div/ul/li[1]').click()
+    await page.wait_for_timeout(200)
+    add_loggs('name', 1)
+    await page.fill('#firstName', first_name)
+    await page.wait_for_timeout(200)
+    await page.fill('#lastName', last_name)
+    await page.wait_for_timeout(200)
+    await page.click('.VfPpkd-LgbsSe')
+    await page.wait_for_timeout(200)
+    add_loggs('date', 1)
+    await page.fill('#day', day)
+    await page.wait_for_timeout(200)
+    await page.select_option('#month', index=month)
+    await page.wait_for_timeout(200)
+    await page.fill('#year', year)
+    await page.wait_for_timeout(200)
+    await page.select_option('#gender', index=sex)
+    await page.wait_for_timeout(200)
+    await page.click('.VfPpkd-LgbsSe')
+    await page.wait_for_timeout(4000)
+    add_loggs('gmail', 1)
+    try:
+        await page.fill('input[name="Username"]', gmail, timeout=500)
+        await page.wait_for_timeout(200)
+        await page.click('#next', timeout=100)
+    except PlaywrightTimeoutError as e:
+        await asyncio.sleep(1)
+        logging.critical(e)
+        await page.click(
+            'xpath=/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[2]/div/div/div/form/span/section/div/div/div[1]/div[1]/div/span/div[3]/div/div[1]/div/div[3]/div')
+        await page.wait_for_timeout(200)
+        await page.fill('input[name="Username"]', gmail)
+        await page.wait_for_timeout(200)
+        await page.click('#next')
+    add_loggs('pass', 1)
+    await page.wait_for_timeout(2000)
+    await page.fill('input[name="Passwd"]', password)
+    await page.wait_for_timeout(200)
+    await page.fill('input[name="PasswdAgain"]', password)
+    await page.wait_for_timeout(200)
+    await page.click('.VfPpkd-LgbsSe')
+    await page.wait_for_timeout(5000)
+    add_loggs('phone', 1)
+    try:
+        await page.fill('#phoneNumberId', phone_string, timeout=10000)
+        await page.wait_for_timeout(200)
+        await page.click('.VfPpkd-LgbsSe')
+        await page.wait_for_timeout(200)
+    except PlaywrightTimeoutError as e:
+        logging.critical(e)
+        return 0
+    for r in range(150):
+        sms = await get_sms(phone_jd)
+        logging.critical(sms)
+        if sms != '{"messages":[]}':
+            break
+        await asyncio.sleep(0.2)
+    pattern = r'\d+'
+    sms = re.findall(pattern, sms)
+    sms = ' '.join(sms)
+    logging.critical(sms)
+
+    await page.wait_for_timeout(3000)
+    await page.fill('#code', sms)
+    await page.wait_for_timeout(3000)
+    await page.click('.VfPpkd-LgbsSe')
+    await page.wait_for_timeout(3000)
+    await page.click('xpath=/html/body/div[1]/div[1]/div[2]/div/div/div[3]/div/div[1]/div[2]/div/div/button')
+    await page.wait_for_timeout(5000)
+    await page.click('.VfPpkd-LgbsSe')
+    await page.wait_for_timeout(5000)
+    await page.click('xpath=/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button')
+    await page.wait_for_timeout(10000)
+    cookies = await context.cookies()
+    cookie_dict = {cookie['name']: cookie['value'] for cookie in cookies}
+    await page.screenshot(path="screenshot.png")
+    birthday = users["birth_date"]
+    acc = str(phone_jd) + ' ' + password + ' ' + proxy + first_name + last_name + str(birthday) + str(humanoid_id)
+    add_loggs(acc, 1)
+    response = await send_acc(phone_jd, password, proxy, first_name, last_name, birthday, humanoid_id, cookie_dict)
+    url = 'http://10.9.20.135:3000/phones/' + str(phone_jd['phone']) + '/link?'
+    data = {'service': 'gmail'}
+    claim = await make_request('get', url, params=data)
+    logging.critical(claim)
+    return response
+
+def add_loggs(message, id_log):
+    DBC.execute('INSERT INTO "Testing".logs(log, id_log) VALUES (%s, %s)', (message, id_log))
+    DB.commit()
 
 
 if __name__ == "__main__":
