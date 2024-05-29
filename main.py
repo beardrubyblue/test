639977cd6d6a9eea45f908c1bcad4ab9f2382b27
AlanD <avlasenko@arbat.dev>
2024-05-29 18:44:12 +0300

    update

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ async def create_proxy_list(kind: int = 3, ptype: str = 3, country: str = 'RU', 
                     proxy_list.append(f'{pt}{tds[0].text.strip()}:{tds[1].text.strip()}')
     # Получение платных https или socks5 прокси указанной страны из объединения proxy6_net_pool сайта [https://proxy-manager.arbat.dev].
     if kind == 2 and ptype in [2, 3]:
-        params = {'pool_id': '9f687b07-b5f5-4227-9d04-4888ac5be496', 'limit': 10000, 'sla': '0.17'}
+        params = {'pool_id': '9f687b07-b5f5-4227-9d04-4888ac5be496', 'limit': 10000, 'sla': '0'}
         async with aiohttp.ClientSession() as session:
             response = await session.get('https://proxy-manager.arbat.dev/pools/9f687b07-b5f5-4227-9d04-4888ac5be496/proxies', params=params)
             jd = json.loads(await response.text(errors='replace'))
