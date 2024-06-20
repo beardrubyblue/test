0d351273c7cdd1cfd4430c847f97344b7dc2dad4
AlanD <avlasenko@arbat.dev>
2024-06-20 19:58:08 +0300

    patch

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ COPY requirements.txt .
 RUN pip install -r requirements.txt
 RUN playwright install-deps
 RUN playwright install firefox
+RUN playwright install chromium
 COPY . .
 ENTRYPOINT ["python", "main.py"]
--- main.py
+++ main.py
@@ -732,7 +732,7 @@ async def email_register(count: Optional[int] = None):
             await standart_request('get', f'https://accman-odata.arbat.dev/get-innocent-humanoid?kind_id={MAIL_KIND_ID}'))
 
         async with async_playwright() as playwright:
-            chromium = playwright.firefox
+            chromium = playwright.chromium
             browser = await chromium.launch()
             context = await browser.new_context(proxy=proxy)
             page = await context.new_page()
