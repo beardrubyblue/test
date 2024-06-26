4eb9a1d39ac1d600284c44a96ac2cbdd7b785257
AlanD <ultrabober@gmail.com>
2024-06-26 13:09:16 +0300

    good

--- main.py
+++ main.py
@@ -329,12 +329,12 @@ def vk_revive_access_token(phone_string: str, password: str, credentials: HTTPBa
 
 
 @app.get("/vk-execute-api-method")
-def vk_execute_api_method(account_id: int = 51, api_method: str = 'https://api.vk.com/method/groups.get', v: str = '5.154', ids: str = '1,2,3,4,5,6,7,8,9,10', credentials: HTTPBasicCredentials = Depends(SECURITY)):
+def vk_execute_api_method(account_id: int = 51, api_method: str = 'https://api.vk.com/method/groups.getById', v: str = '5.154', groups_ids: str = '1,2,3,4,5,6,7,8,9,10', credentials: HTTPBasicCredentials = Depends(SECURITY)):
     """Выполнение API методов ВК."""
     if credentials.username != 'AlanD' or credentials.password != 'Bober666':
         return HTMLResponse(content='В доступе отказано!', status_code=200)
     at = asyncio.run(standart_execute_sql(f"select info->>'access_token' from accounts where id={account_id}"))
-    html = asyncio.run(standart_request('post', api_method, data={'user_ids': ids, 'access_token': at[0], 'v': v}))
+    html = asyncio.run(standart_request('post', api_method, data={'groups_ids': ids, 'access_token': at[0], 'v': v}))
     return HTMLResponse(content=html, status_code=200)
 
 
