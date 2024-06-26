3dcdb8bf2acad07e7af06d4771f4c7273853e467
AlanD <ultrabober@gmail.com>
2024-06-26 13:16:03 +0300

    good

--- main.py
+++ main.py
@@ -334,6 +334,7 @@ def vk_execute_api_method(account_id: int = 51, api_method: str = 'https://api.v
     if credentials.username != 'AlanD' or credentials.password != 'Bober666':
         return HTMLResponse(content='В доступе отказано!', status_code=200)
     at = asyncio.run(standart_execute_sql(f"select info->>'access_token' from accounts where id={account_id}"))
+    html = 'Try Another Method please. A ha Ha ha ha HAAAA !!! :)'
     if api_method == 'https://api.vk.com/method/groups.getById':
         html = asyncio.run(standart_request('post', api_method, data={'group_ids': ids, 'access_token': at[0], 'v': v}))
     return HTMLResponse(content=html, status_code=200)
