ea5d43d32c92dd18d568aec97e85bff62fdd2c21
aland <ultrabober@gmail.com>
2024-10-11 12:59:54 +0300

    patch

--- main.py
+++ main.py
@@ -327,7 +327,7 @@ def get_access_token(phone_string: str, password: str):
         except Exception as e:
             logging.critical(e)
 
-
+app.mount("/", StaticFiles(directory="ui", html=True), name="ui")
 @app.get("/vk-revive-access-token")
 def vk_revive_access_token(phone_string: str, password: str, credentials: HTTPBasicCredentials = Depends(SECURITY)):
     """Воскрешение доступа к учётной записи ВК."""
