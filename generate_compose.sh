8bee3296da84a5c86448efefb1478ff1eaea9299
AlanD <avlasenko@arbat.dev>
2024-10-28 21:56:01 +0300

    patch

--- generate_compose.sh
+++ generate_compose.sh
@@ -18,10 +18,11 @@ services:
 EOL
 
 for i in $(seq 1 $CONTAINER_COUNT); do
-  mkdir -p ./app/unireger$i
   cat <<EOL >> $COMPOSE_FILE
   unireger$i:
     <<: *defaults
+    working_dir: /app/unireger$i
+    command: ["python", "app/main.py"]
     secrets:
       - secret1
       - secret2
