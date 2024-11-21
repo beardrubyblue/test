9e9137089b5665f5b4b46c6aeca0a4f09fdfcab3
AlanD <ultrabober@gmail.com>
2024-11-21 10:21:40 +0300

    PROJECT_NAME from slots

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ YANDEX_KIND_ID = 50
 RAMBLER_KIND_ID = 31
 REGISTRATION_STARTED = False
 random.seed()
-PROJECT_NAME = os.getenv('CONTAINER_NAME')
+PROJECT_NAME = 'UniReger' + os.getenv('CONTAINER_NAME')
 logging.critical(f"Project Name: {PROJECT_NAME}")
 APP = FastAPI(title='UniReger')
 APP.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
