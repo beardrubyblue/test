4be18969e9990082ea79776f5c8dafc73cc998b4
AlanD <avlasenko@arbat.dev>
2025-05-26 18:58:36 +0300

    feat: Add FastApi method - Vk-reger mobile playwright

--- main.py
+++ main.py
@@ -1590,7 +1590,7 @@ async def vk_register_mobile_new(count: Optional[int] = None):
         async with async_playwright() as playwright:
             iphone_13 = playwright.devices['iPhone 13']
             chromium = playwright.chromium
-            browser = await chromium.launch(headless=False)
+            browser = await chromium.launch(headless=True)
             context = await browser.new_context(**iphone_13, proxy=proxy, java_script_enabled=True)
             page = await context.new_page()
             account = await vk_registeration_mobile_new(context, page)
