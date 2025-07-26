a73784645c337b47e7bdd492ef54ae3959f89379
AlaD <avlasenko@arbat.dev>
2025-07-26 03:56:20 +0300

    feat: vk api methods receiver

--- main.py
+++ main.py
@@ -1792,7 +1792,7 @@ async def vk_registeration_mobile_new(context, page):
 @APP.get("/check-vk-api")
 def check_vk_api():
     try:
-        result = asyncio.run(vkapi.run(return_json=True))
+        result = asyncio.run(vkapi.run(return_json=False))
         return JSONResponse(content=result)
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
